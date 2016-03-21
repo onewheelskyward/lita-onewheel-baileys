bebde8febc28bd31376c1b8fe7775abc53b2099f
Andrew Kreps <andrew.kreps@gmail.com>
2016-03-20 23:55:09 -0700

    Nearly there on abv matching.

--- lib/lita/handlers/onewheel_baileys.rb
+++ lib/lita/handlers/onewheel_baileys.rb
@@ -10,7 +10,7 @@ module Lita
             command: true,
             help: {'taps' => 'Display the current taps at baileys.'}
 
-      route /^taps (\w+)$/i,
+      route /^taps ([<>\w.]+)$/i,
             :taps_deets,
             command: true,
             help: {'taps 4' => 'Display the tap 4 deets, including prices.'}
@@ -34,20 +34,29 @@ module Lita
         beers.each do |tap, datum|
           query = response.matches[0][0].strip
           # Search directly by tap number OR full text match.
-          if (query.match(/^\d+$/) and tap == query) or (datum[:search].match(/#{query}/i))
-             Lita.logger.info "Adding tap #{tap} to the list.  #{query.match(/^\d+$/) and tap == query} or #{datum[:search].match(/#{query}/i)}"
-             Lita.logger.info "search field #{datum[:search]}"
-             reply = "Bailey's tap #{tap}) "
-             reply += "#{datum[:brewery]} "
-             reply += "#{datum[:name]} - "
-             reply += "#{datum[:desc]}, "
-             # reply += "Served in a #{datum[1]['glass']} glass.  "
-             reply += "#{datum[:prices]}, "
-             reply += "#{datum[:remaining]}"
-             Lita.logger.info "Replying with #{reply}"
-             response.reply reply
+          Lita.logger.info "Adding tap #{tap} to the list.  #{query.match(/^\d+$/) and tap == query} or #{datum[:search].match(/#{query}/i)}"
+          if tap == query or (query =~ /nitro/i and tap.match('Nitro')) or (query =~ /cask/i and tap.match('Cask'))
+            send_response(tap, datum, response)
+          end
+          if (abv_matches = query.match(/([><]\d+\.*\d*)/))
+            Lita.logger.info "abv_matches #{abv_matches[0]}"
+            Lita.logger.info "#{datum[:abv].to_f} >= #{abv_matches[0].to_f} == #{datum[:abv].to_f >= abv_matches[0].to_f}"
+            if abv_matches[0] =~ />/ and datum[:abv].to_f >= abv_matches[0].to_f
+              send_response(tap, datum, response)
+            end
           end
         end
+      end
+
+      def send_response(tap, datum, response)
+        reply = "Bailey's tap #{tap}) "
+        reply += "#{datum[:brewery]} "
+        reply += "#{datum[:beer]}"
+        reply += "#{datum[:desc]}, "
+        # reply += "Served in a #{datum[1]['glass']} glass.  "
+        reply += "#{datum[:prices]}, "
+        reply += "#{datum[:remaining]}"
+        response.reply reply
       end
 
       def get_baileys
@@ -61,23 +70,35 @@ module Lita
         noko = Nokogiri.HTML response
         noko.css('div#boxfielddata').each do |m|
           # gimme_what_you_got
+          tap_info = {}
+          # Lita.logger.info "span first: #{m.css('span').first}"
           tap = m.css('span').first.children.first.to_s.match(/[\w ]+\:/).to_s.sub /\:$/, ''
           remaining = m.attributes['title']
-          brewery = m.css('span a').first.children.to_s.gsub(/\n/, '')
-          brewery.gsub! /RBBA/, ''
-          brewery.strip!
-          beer_name = m.css('span i').first.children.to_s.strip
-          beer_desc_matchdata = m.to_s.gsub(/\n/, '').match(/(<br\s*\/*>)(.+%) /)
-          beer_desc = beer_desc_matchdata[2].gsub(/\s+/, ' ').strip
+          # Lita.logger.info "brewery first: #{m.css('span a').first}"
+          if m.css('span a').first
+            brewery = m.css('span a').first.children.to_s.gsub(/\n/, '')
+            brewery.gsub! /RBBA/, ''
+            brewery.strip!
+          end
+          beer_name = m.css('span i').first.children.to_s
+          if beer_desc_matchdata = m.to_s.gsub(/\n/, '').match(/(<br\s*\/*>)(.+%) /)
+            beer_desc = beer_desc_matchdata[2].gsub(/\s+/, ' ').strip
+            if (abv_matches = beer_desc.match(/\d+\.\d+%/))
+              # Lita.logger.info "#{abv_matches.inspect}"
+              abv = abv_matches.to_s.sub '%', ''
+              Lita.logger.info "ABV: #{abv}"
+            end
+          end
           prices_str = m.css('div#prices').children.to_s.strip
           prices = Sanitize.clean(prices_str).gsub(/We're Sorry/, '').gsub(/Inventory Restriction/, '').gsub(/Inventory Failure/, '').gsub('Success!', '').gsub(/\s+/, ' ').strip
           full_text_search = "#{tap.sub /\d+/, ''} #{brewery} #{beer_name} #{beer_desc.gsub /\d+\.*\d*%*/, ''}"
 
           gimme_what_you_got[tap] = {
               remaining: remaining,
-              brewery: brewery,
+              brewery: brewery.to_s,
               name: beer_name,
-              desc: beer_desc,
+              desc: beer_desc.to_s,
+              abv: abv,
               prices: prices,
               search: full_text_search
           }
