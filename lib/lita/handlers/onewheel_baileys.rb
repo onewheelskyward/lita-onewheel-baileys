130fbdeaaf71ff0e3828e173f3f7646d8855d5d9
Andrew Kreps <andrew.kreps@gmail.com>
2016-03-22 11:17:09 -0700

    Refactored info build function, started price search.

--- lib/lita/handlers/onewheel_baileys.rb
+++ lib/lita/handlers/onewheel_baileys.rb
@@ -20,6 +20,11 @@ module Lita
             command: true,
             help: {'taps >4%' => 'Display beers over 4% ABV.'}
 
+      route /^taps ([<>\$\w.]+)$/i,
+            :taps_by_price,
+            command: true,
+            help: {'taps <$5' => 'Display beers under $5.'}
+
       def taps_list(response)
         beers = get_baileys
         reply = "Bailey's taps: "
@@ -66,6 +71,27 @@ module Lita
         end
       end
 
+      def taps_by_price(response)
+        beers = get_baileys
+        beers.each do |tap, datum|
+          if datum[:abv].to_f == 0.0
+            next
+          end
+          query = response.matches[0][0].strip
+          # Search directly by tap number OR full text match.
+          if (abv_matches = query.match(/([><])(\d+\.*\d*)/))
+            direction = abv_matches.to_s.match(/[<>]/).to_s
+            abv_requested = abv_matches.to_s.match(/\d+.*\d*/).to_s
+            if direction == '>' and datum[:abv].to_f >= abv_requested.to_f
+              send_response(tap, datum, response)
+            end
+            if direction == '<' and datum[:abv].to_f <= abv_requested.to_f
+              send_response(tap, datum, response)
+            end
+          end
+        end
+      end
+
       def send_response(tap, datum, response)
         reply = "Bailey's tap #{tap}) "
         reply += "#{datum[:brewery]} "
@@ -86,36 +112,24 @@ module Lita
       def parse_response(response)
         gimme_what_you_got = {}
         noko = Nokogiri.HTML response
-        noko.css('div#boxfielddata').each do |m|
+        noko.css('div#boxfielddata').each do |beer_node|
           # gimme_what_you_got
-          tap_info = {}
-          # Lita.logger.info "span first: #{m.css('span').first}"
-          tap = get_tap_name(m)
-          remaining = m.attributes['title']
-          # Lita.logger.info "brewery first: #{m.css('span a').first}"
-          if m.css('span a').first
-            brewery = m.css('span a').first.children.to_s.gsub(/\n/, '')
-            brewery.gsub! /RBBA/, ''
-            brewery.strip!
-          end
-          beer_name = m.css('span i').first.children.to_s
-          if beer_desc_matchdata = m.to_s.gsub(/\n/, '').match(/(<br\s*\/*>)(.+%) /)
-            beer_desc = beer_desc_matchdata[2].gsub(/\s+/, ' ').strip
-            if (abv_matches = beer_desc.match(/\d+\.\d+%/))
-              # Lita.logger.info "#{abv_matches.inspect}"
-              abv = abv_matches.to_s.sub '%', ''
-            end
-          end
-          prices_str = m.css('div#prices').children.to_s.strip
-          prices = Sanitize.clean(prices_str).gsub(/We're Sorry/, '').gsub(/Inventory Restriction/, '').gsub(/Inventory Failure/, '').gsub('Success!', '').gsub(/\s+/, ' ').strip
+          tap = get_tap_name(beer_node)
+          remaining = beer_node.attributes['title']
+
+          brewery = get_brewery(beer_node)
+          beer_name = beer_node.css('span i').first.children.to_s
+          beer_desc = get_beer_desc(beer_node)
+          abv = get_abv(beer_desc)
           full_text_search = "#{tap.sub /\d+/, ''} #{brewery} #{beer_name} #{beer_desc.to_s.gsub /\d+\.*\d*%*/, ''}"
+          prices = get_prices(beer_node)
 
           gimme_what_you_got[tap] = {
               remaining: remaining,
               brewery: brewery.to_s,
-              name: beer_name,
+              name: beer_name.to_s,
               desc: beer_desc.to_s,
-              abv: abv,
+              abv: abv.to_f,
               prices: prices,
               search: full_text_search
           }
@@ -123,8 +137,55 @@ module Lita
         gimme_what_you_got
       end
 
-      def get_tap_name(m)
-        m.css('span').first.children.first.to_s.match(/[\w ]+\:/).to_s.sub /\:$/, ''
+      def get_abv(beer_desc)
+        if (abv_matches = beer_desc.match(/\d+\.\d+%/))
+          abv_matches.to_s.sub '%', ''
+        end
+      end
+
+      # Return the desc of the beer, "Amber ale 6.9%"
+      def get_beer_desc(noko)
+        beer_desc = ''
+        if (beer_desc_matchdata = noko.to_s.gsub(/\n/, '').match(/(<br\s*\/*>)(.+%) /))
+          beer_desc = beer_desc_matchdata[2].gsub(/\s+/, ' ').strip
+        end
+        beer_desc
+      end
+
+      # Get the brewery from the node, return it or blank.
+      def get_brewery(noko)
+        brewery = ''
+        if (node = noko.css('span a').first)
+          brewery = node.children.to_s.gsub(/\n/, '')
+          brewery.gsub! /RBBA/, ''
+          brewery.strip!
+        end
+        brewery
+      end
+
+      # Returns ...
+      # There are a bunch of hidden html fields that get stripped after sanitize.
+      def get_prices(noko)
+        prices_str = noko.css('div#prices').children.to_s.strip
+        Sanitize.clean(prices_str)
+            .gsub(/We're Sorry/, '')
+            .gsub(/Inventory Restriction/, '')
+            .gsub(/Inventory Failure/, '')
+            .gsub('Success!', '')
+            .gsub(/\s+/, ' ')
+            .strip
+      end
+
+      # Returns 1, 2, Cask 3, Nitro 4...
+      def get_tap_name(noko)
+        noko.css('span')
+            .first
+            .children
+            .first
+            .to_s
+            .match(/[\w ]+\:/)
+            .to_s
+            .sub(/\:$/, '')
       end
 
       Lita.register_handler(self)
