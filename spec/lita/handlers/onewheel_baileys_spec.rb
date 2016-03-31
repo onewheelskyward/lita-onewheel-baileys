8ac8fed34e93e97f1559aa95266edf1cce9f2589
Andrew Kreps <andrew.kreps@gmail.com>
2016-03-31 01:01:36 -0700

    Adding test coverage

--- spec/lita/handlers/onewheel_baileys_spec.rb
+++ spec/lita/handlers/onewheel_baileys_spec.rb
@@ -144,6 +144,14 @@ describe Lita::Handlers::OnewheelBaileys, lita_handler: true do
     expect(replies[2]).to eq("Bailey's tap 20) Knee Deep Dark Horse - Imperial Stout 12.0%, 4oz - $2 | 12oz - $4 | 32oz Crowler - $10, 39% remaining")
   end
 
+  it 'searches for prices <= $4.00' do
+    send_command 'taps <= $4.00'
+    expect(replies.count).to eq(3)
+    expect(replies[0]).to eq("Bailey's tap 8) Fat Head’s Zeus Juice - Belgian Strong Blonde 10.0%, 4oz - $2 | 12oz - $4 | 32oz Crowler - $9, 44% remaining")
+    expect(replies[1]).to eq("Bailey's tap 18) Knee Deep Hop Surplus - Triple IPA 10.0%, 4oz - $2 | 12oz - $4 | 32oz Crowler - $10, 25% remaining")
+    expect(replies[2]).to eq("Bailey's tap 20) Knee Deep Dark Horse - Imperial Stout 12.0%, 4oz - $2 | 12oz - $4 | 32oz Crowler - $10, 39% remaining")
+  end
+
   it 'runs a random beer through' do
     send_command 'taps roulette'
     expect(replies.count).to eq(1)
